2753fc585152c6fedaa0679e99f510fa2e56e697
Edward Raigosa <wenlock@hp.com>
2014-08-21 23:44:05 +0000

    add startup options

--- test-bot.sh
+++ test-bot.sh
@@ -4,8 +4,8 @@ then
   echo "Killing previous process... ($(pgrep node))"
   pkill node
 fi
+
 cd $(dirname $0)
-[ -f ./creds.env ] && . ./creds.env
 [ -f ./admins.env ] && . ./admins.env
 echo 'starting nina-bot'
-nohup bin/hubot -n nina-bot -a xmpp >> /var/log/hubot.log 2>&1 &
+bin/hubot -n nina-bot -a shell
