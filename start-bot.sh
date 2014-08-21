9c16dfbe4fdad87622371cf650f5f5415d5489ed
Ubuntu <ubuntu@chat.(none)>
2014-08-21 22:17:15 +0000

    fixing startup

--- start-bot.sh
+++ start-bot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd $(dirname $0)
 . ./creds.env
+if [ "$(pgrep node)" != "" ]
+then
+    echo "Killing previous process... ($(pgrep node))"
+    pkill node
+fi
 echo 'starting nina-bot'
 nohup bin/hubot -n nina-bot -a xmpp >> /var/log/hubot.log 2>&1 &
