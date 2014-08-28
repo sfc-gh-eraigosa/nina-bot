2576944d7d02336550c64880eae678d15c5dcb48
Edward Raigosa <wenlock@hp.com>
2014-08-28 18:37:04 +0000

    remote bot start

---
+++ remote-start-bot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# our goal is to connect to an ssh server
+# as defined by the current git remote for origin
+# and run start.sh
+SSH_ALIAS=$(git remote -v|grep -e '^origin.*'|grep -e '(fetch)$'|awk '{print $2}'|awk -F: '{print $1}')
+SSH_PATH=$(git remote -v|grep -e '^origin.*'|grep -e '(fetch)$'|awk '{print $2}'|awk -F: '{print $2}')
+ssh -T $SSH_ALIAS exit > /dev/null 2<&1
+if [ $? -eq 0 ] ; then
+  echo "attempting to perform bot start"
+  ssh $SSH_ALIAS bash -c $SSH_PATH/start-bot.sh
+  if [ $? -eq 0 ] ; then
+    echo "bot start success"
+  else
+    echo "bot failed to start"
+    exit 1
+  fi
+else
+  echo "unable to perform bot start"
+fi
+
